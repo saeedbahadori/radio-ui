98ea988083e466f9cd4226afe35bd46c7b889a97
saeedbahadori <saeedbahadori.ir@gmail.com>
2026-02-17 22:08:28 +0330

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,6 @@
-import http.server
-import socketserver
-import os
-
-PORT = int(os.environ.get("PORT", 8080))
-
-# go to project directory explicitly
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-Handler = http.server.SimpleHTTPRequestHandler
-
-class ReusableTCPServer(socketserver.TCPServer):
-    allow_reuse_address = True
-
-with ReusableTCPServer(("0.0.0.0", PORT), Handler) as httpd:
-    print(f"Serving UI on port {PORT}")
-    httpd.serve_forever()
+{
+  "$schema": "https://railway.app/railway.schema.json",
+  "deploy": {
+    "startCommand": "python server.py"
+  }
+}
