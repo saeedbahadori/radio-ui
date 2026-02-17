f487e911cb8e09c2153f97b94aaa6f84b766b508
saeedbahadori <saeedbahadori.ir@gmail.com>
2026-02-17 22:06:44 +0330

    Update server.py

--- server.py
+++ server.py
@@ -4,8 +4,14 @@ import os
 
 PORT = int(os.environ.get("PORT", 8080))
 
+# go to project directory explicitly
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
+class ReusableTCPServer(socketserver.TCPServer):
+    allow_reuse_address = True
+
+with ReusableTCPServer(("0.0.0.0", PORT), Handler) as httpd:
     print(f"Serving UI on port {PORT}")
     httpd.serve_forever()
