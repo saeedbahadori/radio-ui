b3359232ad1006c1a5930cd5fb41ca7677828bae
saeedbahadori <saeedbahadori.ir@gmail.com>
2026-02-18 02:48:18 +0330

    Update server.py

--- server.py
+++ server.py
@@ -7,5 +7,5 @@ PORT = int(os.environ.get("PORT", 8080))
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
-    print("Serving on port", PORT)
+    print(f"Serving on port {PORT}")
     httpd.serve_forever()
