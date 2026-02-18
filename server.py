036750987e99e99c44d82205736f188481750dfc
saeedbahadori <saeedbahadori.ir@gmail.com>
2026-02-18 03:44:57 +0330

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
+import os
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 @app.get("/")
 def home():
-    return FileResponse("index.html")
+    return FileResponse(os.path.join(BASE_DIR, "index.html"))
