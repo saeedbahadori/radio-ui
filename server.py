02a1a5a3b716b8cc0e340fb06e7aa4206fd6a51f
saeedbahadori <saeedbahadori.ir@gmail.com>
2026-02-18 03:52:11 +0330

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+import uvicorn
 
 app = FastAPI()
 
@@ -9,3 +10,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 @app.get("/")
 def home():
     return FileResponse(os.path.join(BASE_DIR, "index.html"))
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
